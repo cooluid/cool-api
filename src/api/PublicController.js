dd3de4ae2ea0fa700ccb3d2a20fb81f7af4fd563
fr <400822@163.com>
2019-12-06 00:48:32 +0800

    图形验证码

--- src/api/PublicController.js
+++ src/api/PublicController.js
@@ -5,8 +5,16 @@ class PublicController {
 	}
 
 	async getCaptcha(ctx) {
-		const newCaptcha = svgCaptcha.create({})
-		console.log(newCaptcha.data)
+		let newCaptcha;
+		newCaptcha = svgCaptcha.create({
+			size: 4,
+			color: true,
+			ignoreChars: '1il0oz2',
+			width: 150,
+			height: 50,
+			noise: Math.floor(Math.random() * 5)
+		});
+		console.log(newCaptcha.data);
 		ctx.body = {
 			code: 200,
 			data: newCaptcha.data,
