c4a64c93c700bd926c358eb83e80ea4523e80821
fr <400822@163.com>
2019-12-06 00:13:35 +0800

    init

---
+++ src/api/PublicController.js
@@ -0,0 +1,17 @@
+import svgCaptcha from 'svg-captcha'
+
+class PublicController {
+	constructor() {
+	}
+
+	async getCaptcha(ctx) {
+		const newCaptcha = svgCaptcha.create({})
+		console.log(newCaptcha.data)
+		ctx.body = {
+			code: 200,
+			data: newCaptcha.data,
+		}
+	}
+}
+
+export default new PublicController()
