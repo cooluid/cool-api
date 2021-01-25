292d4775cdb9cc4723fb2eb429a39e9b9ef86ad8
cooluid <173000@qq.com>
2021-01-25 19:26:52 +0800

    路由器优化

--- src/model/User.js
+++ src/model/User.js
@@ -9,12 +9,12 @@ const UserSchema = new Schema({
 	created: { type: Date }, //	注册时间
 	favs: { type: Number, default: 100 }, //	积分
 	updated: { type: Date }, //	更新时间
-	gender: { type: String, default: 0 }, //性别：0-男 1-女
+	gender: { type: String, default: "" }, //性别：0-男 1-女
 	roles: { type: Array, default: ["user"] }, //user	角色：user-普通用户 admin-管理员 super_admin-超级管理员
 	pic: { type: String, default: "/img/avatar.png" }, //用户头像
 	mobile: { type: Number, match: /^1[3-9](\d{9})$/, default: "" }, //	用户手机号码
 	status: { type: Number, default: 0 }, //	是被//禁用 0-正常 1-禁言 2-账号禁用
-	regmark: { type: String }, //	个性签名
+	regmark: { type: String, default: "" }, //	个性签名
 	location: { type: String }, //	城市
 	isVip: { type: Number, default: 0 }, //VIP用户: 0-普通用户 1-vip用户
 	count: { type: Number, default: 0 }, //签到次数
